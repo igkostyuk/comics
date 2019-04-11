2e4c7d5b210fe4fe69e85f90d34a190824b31d90
igor Kostiuk <ig.fire@gmail.com>
2019-04-11 17:25:04 +0300

    Updates

---
+++ precache-manifest.1ef182fc2888c1b14c2194c661887f4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "90f5b483a13fb54a0f90",
+    "url": "/comics/static/css/main.9b2490a9.chunk.css"
+  },
+  {
+    "revision": "90f5b483a13fb54a0f90",
+    "url": "/comics/static/js/main.3b1a6a31.chunk.js"
+  },
+  {
+    "revision": "57d136ffd59e2ac7898b",
+    "url": "/comics/static/js/runtime~main.14d91cc0.js"
+  },
+  {
+    "revision": "ac57ea34d1ddfed19b74",
+    "url": "/comics/static/js/2.09780357.chunk.js"
+  },
+  {
+    "revision": "34068f262c970248321dc0912fbb9c48",
+    "url": "/comics/static/media/GravityAnimating.34068f26.svg"
+  },
+  {
+    "revision": "02fbde0488078a2ea6f5ae3ba08ec699",
+    "url": "/comics/index.html"
+  }
+];
