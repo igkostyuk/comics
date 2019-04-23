a609360f2d11b3b2ccd5c713c1a4f2978b876a64
igor Kostiuk <ig.fire@gmail.com>
2019-04-23 09:29:41 +0300

    Updates

---
+++ precache-manifest.3a7d288806fa5e1850d6e137088ca4a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9c0e93065025869fb79",
+    "url": "/comics/static/css/main.29532e73.chunk.css"
+  },
+  {
+    "revision": "b9c0e93065025869fb79",
+    "url": "/comics/static/js/main.8d3c7f12.chunk.js"
+  },
+  {
+    "revision": "57d136ffd59e2ac7898b",
+    "url": "/comics/static/js/runtime~main.14d91cc0.js"
+  },
+  {
+    "revision": "c2c5fb3e6364eda8327b",
+    "url": "/comics/static/js/2.f1214f14.chunk.js"
+  },
+  {
+    "revision": "34068f262c970248321dc0912fbb9c48",
+    "url": "/comics/static/media/GravityAnimating.34068f26.svg"
+  },
+  {
+    "revision": "b33e6e1bacce8b50b149e696e84135d6",
+    "url": "/comics/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/comics/precache-manifest.dba0b3f9ccbc88b189234eec83ddf0bf.js"
+  "/comics/precache-manifest.3a7d288806fa5e1850d6e137088ca4a7.js"
 );
 
 workbox.clientsClaim();
