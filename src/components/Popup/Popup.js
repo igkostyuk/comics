0e5ab39a2a945e6c0ba90d6227de200c086ac68e
igor Kostiuk <ig.fire@gmail.com>
2019-04-24 17:53:39 +0300

    fix

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './style.scss';
 
-const Popup = ({ closePopup, comics, title, image }) => (
+const Popup = ({ closePopup, comics }) => (
   <div className="popup">
     <div className="popup_inner">
-      <div className="popup_inner-left">{image}</div>
+      <div className="popup_inner-left">
+        <img
+          src={`${comics.thumbnail.path}.${comics.thumbnail.extension}`}
+          alt={comics.title}
+        />
+      </div>
       <div className="popup_inner-right">
-        {title}
+        <h2>{comics.title}</h2>
         <p>{comics.description}</p>
         <div className="popup_inner-links">
           {comics.urls.map((link, index) => (
